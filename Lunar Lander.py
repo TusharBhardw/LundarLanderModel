7def33077925902fe4cb3f76f80a0e657e05f1a1
Tushar Bhardwaj <108877526+TusharBhardw@users.noreply.github.com>
2024-03-21 23:29:29 +0530

    Add files via upload

---
+++ Lunar Lander.py
@@ -0,0 +1,250 @@
+import numpy as np
+import pandas as pd
+
+!pip install swig
+
+!pip install utils
+
+!pip install pyvirtualdisplay
+
+!pip install gym[all]
+
+import time
+from collections import deque, namedtuple
+
+import gym
+import numpy as np
+import PIL.Image
+import tensorflow as tf
+import utils
+import matplotlib.pyplot as plt
+
+
+from pyvirtualdisplay import Display
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.losses import MSE
+from tensorflow.keras.optimizers import Adam
+
+!apt-get install xvfb
+
+# Set up a virtual display to render the Lunar Lander environment.
+Display(visible=0, size=(840, 480)).start();
+
+# Set the random seed for TensorFlow
+seed_value = int(time.time())
+tf.random.set_seed(seed_value)
+np.random.seed(seed_value)
+
+MEMORY_SIZE = 100_000     # size of memory buffer
+GAMMA = 0.995             # discount factor
+ALPHA = 1e-3              # learning rate
+NUM_STEPS_FOR_UPDATE = 4  # perform a learning update every C time steps
+
+env = gym.make("LunarLander-v2", render_mode="human")
+
+state_size = env.observation_space.shape
+num_actions = env.action_space.n
+
+print('State Shape:', state_size)
+print('Number of actions:', num_actions)
+
+initial_state = env.reset()
+
+action = 0
+print(env.step(action))
+next_state, reward, done, info= env.step(action)
+
+with np.printoptions(formatter={'float': '{:.3f}'.format}):
+    print("Initial State:", initial_state)
+    print("Action:", action)
+    print("Next State:", next_state)
+    print("Reward Received:", reward)
+    print("Episode Terminated:", done)
+    print("Info:", info)
+
+q_network = Sequential([
+    Input(shape=state_size),
+    Dense(units=64, activation='relu'),
+    Dense(units=64, activation='relu'),
+    Dense(units=num_actions, activation='linear'),
+    ])
+
+# Create the target Q^-Network
+target_q_network = Sequential([
+    Input(shape=state_size),
+    Dense(units=64, activation='relu'),
+    Dense(units=64, activation='relu'),
+    Dense(units=num_actions, activation='linear'),
+    ])
+
+### START CODE HERE ###
+optimizer = Adam(learning_rate=ALPHA)
+
+experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+def compute_loss(experiences, gamma, q_network, target_q_network):
+    """
+    Calculates the loss.
+
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+      q_network: (tf.keras.Sequential) Keras model for predicting the q_values
+      target_q_network: (tf.keras.Sequential) Karas model for predicting the targets
+
+    Returns:
+      loss: (TensorFlow Tensor(shape=(0,), dtype=int32)) the Mean-Squared Error between
+            the y targets and the Q(s,a) values.
+    """
+
+    # Unpack the mini-batch of experience tuples
+    states, actions, rewards, next_states, done_vals = experiences
+
+    # Compute max Q^(s,a)
+    max_qsa = tf.reduce_max(target_q_network(next_states), axis=-1)
+
+    # Set y = R if episode terminates, otherwise set y = R + γ max Q^(s,a).
+    ### START CODE HERE ###
+    y_targets = rewards + (gamma * max_qsa * (1 - done_vals))
+    ### END CODE HERE ###
+
+    # Get the q_values
+    q_values = q_network(states)
+    q_values = tf.gather_nd(q_values, tf.stack([tf.range(q_values.shape[0]),
+                                                tf.cast(actions, tf.int32)], axis=1))
+
+    # Compute the loss
+    ### START CODE HERE ###
+    loss = MSE(y_targets, q_values)
+    ### END CODE HERE ###
+
+    return loss
+
+def update_target_network(q_network, target_q_network, tau=0.001):
+    q_network_weights = q_network.get_weights()
+    target_q_network_weights = target_q_network.get_weights()
+
+    for i in range(len(target_q_network_weights)):
+        target_q_network_weights[i] = (1 - tau) * target_q_network_weights[i] + tau * q_network_weights[i]
+
+    target_q_network.set_weights(target_q_network_weights)
+
+@tf.function
+def agent_learn(experiences, gamma):
+    """
+    Updates the weights of the Q networks.
+
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+
+    """
+
+    # Calculate the loss
+    with tf.GradientTape() as tape:
+        loss = compute_loss(experiences, gamma, q_network, target_q_network)
+
+    # Get the gradients of the loss with respect to the weights.
+    gradients = tape.gradient(loss, q_network.trainable_variables)
+
+    # Update the weights of the q_network.
+    optimizer.apply_gradients(zip(gradients, q_network.trainable_variables))
+
+    # update the weights of target q_network
+    update_target_network(q_network, target_q_network)
+
+start = time.time()
+
+num_episodes = 2000
+max_num_timesteps = 1000
+
+total_point_history = []
+
+num_p_av = 100    # number of total points to use for averaging
+epsilon = 1.0     # initial ε value for ε-greedy policy
+
+# Create a memory buffer D with capacity N
+memory_buffer = deque(maxlen=MEMORY_SIZE)
+
+# Set the target network weights equal to the Q-Network weights
+target_q_network.set_weights(q_network.get_weights())
+
+for i in range(num_episodes):
+
+    # Reset the environment to the initial state and get the initial state
+    state = env.reset()
+    total_points = 0
+
+    for t in range(max_num_timesteps):
+        # From the current state S choose an action A using an ε-greedy policy
+        state_qn = np.expand_dims(state,axis=0)
+
+
+ # state needs to be the right shape for the q_network
+        q_values = q_network(state_qn)
+        if np.random.rand() < epsilon:
+          action = np.random.choice(num_actions)
+        else:
+          action = np.argmax(q_values.numpy())
+
+        # Take action A and receive reward R and the next state S'
+        next_state, reward, done, _ = env.step(action)
+
+        # Store experience tuple (S,A,R,S') in the memory buffer.
+        # We store the done variable as well for convenience.
+        memory_buffer.append(experience(state, action, reward, next_state, done))
+
+        # Only update the network every NUM_STEPS_FOR_UPDATE time steps.
+        # Replace utils.check_update_conditions(t, NUM_STEPS_FOR_UPDATE, memory_buffer) with a simple condition
+        update = t % NUM_STEPS_FOR_UPDATE == 0 and len(memory_buffer) >= MEMORY_SIZE
+
+
+        if update:
+            # Sample random mini-batch of experience tuples (S,A,R,S') from D
+            # Replace utils.get_experiences(memory_buffer) with random sampling
+            experiences = random.sample(memory_buffer, BATCH_SIZE)
+            experiences = experience(*zip(*experiences))
+
+
+            # Set the y targets, perform a gradient descent step,
+            # and update the network weights.
+            agent_learn(experiences, GAMMA)
+
+        state = next_state.copy()
+        total_points += reward
+
+        if done:
+            break
+    total_point_history.append(total_points)
+    av_latest_points = np.mean(total_point_history[-num_p_av:])
+
+    # Update the ε value
+    EPSILON_DECAY = 0.55
+    epsilon = max(0.01, epsilon * EPSILON_DECAY)
+
+    print(f"\rEpisode {i+1} | Total point average of the last {num_p_av} episodes: {av_latest_points:.2f}", end="")
+
+    if (i+1) % num_p_av == 0:
+        print(f"\rEpisode {i+1} | Total point average of the last {num_p_av} episodes: {av_latest_points:.2f}")
+
+    # We will consider that the environment is solved if we get an
+    # average of 200 points in the last 100 episodes.
+    if av_latest_points >= 200.0:
+        print(f"\n\nEnvironment solved in {i+1} episodes!")
+        q_network.save('lunar_lander_model.h5')
+        break
+tot_time = time.time() - start
+
+print(f"\nTotal Runtime: {tot_time:.2f} s ({(tot_time/60):.2f} min)")
+
+
+
+
+
+
+
+
+
+
+
